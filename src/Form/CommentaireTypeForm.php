a48ee467c0a83061cd1653a586647d4f14a2d5cb
Norman BELAID <Normanbelaid@gmail.com>
2025-06-26 11:45:58 +0200

    add masque listes

---
+++ src/Form/CommentaireTypeForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Commentaire;
+
+class CommentaireTypeForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('commentaire', TextareaType::class, [
+                'label' => 'Votre commentaire',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaire::class,
+        ]);
+    }
+}
