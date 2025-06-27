375b49b81a27d658b5b9320b943acbab88c21a29
Norman BELAID <Normanbelaid@gmail.com>
2025-06-27 10:20:37 +0200

    add mes masques and css

---
+++ src/Controller/MesMasquesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MasqueRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class MesMasquesController extends AbstractController
+{
+    #[Route('/mes/masques', name: 'app_mes_masques')]
+    public function mesMasques(MasqueRepository $repository): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        
+        $user = $this->getUser();
+        $mesMasques = $repository->findBy(['createdBy' => $user], ['createdAt' => 'DESC']);
+
+        return $this->render('mes_masques/mes_masques.html.twig', [
+            'masques' => $mesMasques,
+        ]);
+    }
+
+}
