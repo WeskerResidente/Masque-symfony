033b2bf0e7f11083b5a2f5188cd535b617de4bc6
Norman BELAID <normanbelaid@gmail.com>
2025-07-15 13:46:07 +0100

    add paswd check

--- src/Service/MessageGenerator.php
+++ src/Service/MessageGenerator.php
@@ -7,19 +7,20 @@ class MessageGenerator
     public function getHappyMessage(): string
     {
         $messages = [
-            'Mystère et élégance en un seul geste.',
-            'Crée. Inspire. Masque-toi',
-            'Un masque, mille émotions.',
-            'Fais sensation, choisis ton masque. ',
-            'Un masque, mille émotions.',
-            ' Révélez-vous autrement. ',
-            ' Derrière chaque masque se cache une histoire unique à révéler.  ',
-            ' Porte un masque, révèle un monde. ',
-            ' Crée ton masque, et laisse ton imagination prendre vie. ',
-            ' Chaque masque est une œuvre d’art qui attend d’être admirée. ',
-            ' Aujourd’hui, choisis d’être qui tu veux grâce à ton masque.  ',
-            ' Ose l’inconnu, ose le masque. ',
-            ' L’art du masque, une tradition qui transcende le temps. ',
+            'Mystère et élégance en un seul geste. ╰(*°▽°*)╯',
+            'Crée. Inspire. Masque-toi ᓚᘏᗢ' ,
+            'Un masque, mille émotions. (￣y▽￣)╭ Ohohoho.....',
+            'Fais sensation, choisis ton masque. ○( ＾皿＾)っ Hehehe…',
+            'Un masque, mille émotions. （づ￣3￣）づ╭❤️～',
+            ' Révélez-vous autrement. ┻━┻ ︵ ＼( °□° )／ ︵ ┻━┻',
+            ' Derrière chaque masque se cache une histoire unique à révéler. ┬┴┬┴┤(･_├┬┴┬┴ ',
+            ' Porte un masque, révèle un monde. (((φ(◎ロ◎;)φ)))',
+            ' Crée ton masque, et laisse ton imagination prendre vie. (＠_＠;) ',
+            ' Chaque masque est une œuvre d’art qui attend d’être admirée. щ(ʘ╻ʘ)щ ',
+            ' Aujourd’hui, choisis d’être qui tu veux grâce à ton masque. ( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°) ',
+            ' Ose l’inconnu, ose le masque. ＼（〇_ｏ）／',
+            ' L’art du masque, une tradition qui transcende le temps. ε=ε=ε=(~￣▽￣)~ ',
+
         ];
 
         $index = array_rand($messages);
